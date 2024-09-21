d9fcc95a877daeb60f46bf5fb0c907b1e405d936
Thomas Stemler <stemlertho@gmail.com>
2024-09-20 20:07:48 -0500

    Added seed data to seeds.sql, and made all the tables in schema.sql actually work

--- db/schema.sql
+++ db/schema.sql
@@ -3,20 +3,20 @@ CREATE DATABASE employees_db;
 
 \c employees_db;
 
-CREATE TABLE department {
+CREATE TABLE department (
     id SERIAL PRIMARY KEY,
-    name VARCHAR(30) UNIQUE NOT NULL
-};
+    department_name VARCHAR(30) UNIQUE NOT NULL
+);
 
-CREATE TABLE role {
+CREATE TABLE role (
     id SERIAL PRIMARY KEY,
     title VARCHAR(30) UNIQUE NOT NULL,
     salary DECIMAL NOT NULL,
     department_id INTEGER NOT NULL,
     FOREIGN KEY (department_id) REFERENCES department(id)
-};
+);
 
-CREATE TABLE employee {
+CREATE TABLE employee (
     id SERIAL PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
@@ -24,4 +24,4 @@ CREATE TABLE employee {
     manager_id INTEGER,
     FOREIGN KEY (role_id) REFERENCES role(id),
     FOREIGN KEY (manager_id) REFERENCES employee(id)
-};
+);
